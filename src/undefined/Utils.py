b94697979849b75ba889f49638f6644f82cf469a
XinranTang <sherrytangssr@gmail.com>
2021-12-08 23:37:15 -0500

    Modify trace(), check shape of user inputs

--- src/undefined/Utils.py
+++ src/undefined/Utils.py
@@ -53,10 +53,10 @@ def check_division_by_zero(val):
     """
     if isinstance(val, np.ndarray):
         if not np.all(val):
-            raise ZeroDivisionError("divide by zero encountered")
+            raise ZeroDivisionError("error raised by undefined: divide by zero encountered")
     elif isinstance(val, (int, float)):
         if val == 0:
-            raise ZeroDivisionError("divide by zero encountered")
+            raise ZeroDivisionError("error raised by undefined: divide by zero encountered")
 
 
 def check_pow(val, degree):
@@ -73,9 +73,9 @@ def check_pow(val, degree):
     try:
         temp = np.power(val, degree)
         if np.any(np.isnan(temp)):
-            raise ValueError("invalid inputs for pow()")
+            raise ValueError("error raised by undefined: invalid inputs for pow()")
     except ValueError as e:
-        raise ValueError(e)
+        raise ValueError(f"error raised by undefined: {e}")
 
 
 def check_log(val, base):
@@ -91,13 +91,13 @@ def check_log(val, base):
         ValueError: [description]
     """
     if base <= 0:
-        raise ValueError(f"invalid base {base} for log")
+        raise ValueError(f"error raised by undefined: invalid base {base} for log")
     if isinstance(val, np.ndarray):
         if np.any(val <= 0):
-            raise ValueError(f"invalid value {val} for log")
+            raise ValueError(f"error raised by undefined: invalid value {val} for log")
     elif isinstance(val, (int, float)):
         if val <= 0:
-            raise ValueError(f"invalid value {val} for log")
+            raise ValueError(f"error raised by undefined: invalid value {val} for log")
 
 
 def check_arc(val):
@@ -113,11 +113,11 @@ def check_arc(val):
     if isinstance(val, np.ndarray):
         if not np.all(val > -1 and val < 1):
             raise ValueError(
-                f"invalid values {val}, which should all be within (-1, 1)")
+                f"error raised by undefined: invalid values {val}, which should all be within (-1, 1)")
     elif isinstance(val, (int, float)):
         if not (val > -1 and val < 1):
             raise ValueError(
-                f"invalid value {val}, which should be within (-1, 1)")
+                f"error raised by undefined: invalid value {val}, which should be within (-1, 1)")
 #     logging.basicConfig(level=logging.INFO)
 #     if level == logging.INFO:
 #         logging.info(information)
