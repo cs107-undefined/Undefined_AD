87a137ce8993cef4a33ed1f0f214a9e35a2c48b6
Renhao Luo <renhao_luo@hms.harvard.edu>
2021-12-09 23:35:11 -0500

    update sphinx directory

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath('../src'))
 
 
 # -- Project information -----------------------------------------------------
@@ -22,7 +22,7 @@ copyright = '2021, Xinran Tang, Renhao Luo, Chelse Swoopes, Shijia Zhang'
 author = 'Xinran Tang, Renhao Luo, Chelse Swoopes, Shijia Zhang'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '2.0.2'
 
 
 # -- General configuration ---------------------------------------------------
